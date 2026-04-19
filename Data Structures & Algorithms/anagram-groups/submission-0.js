f8e1f34ed10af5425899f45455eb728a4d58c7d9
Truman Folkers <37959465+Truman-Folkers@users.noreply.github.com>
2026-04-19 13:25:23 -0500

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+
+        const map = new Map();
+
+    for (let str of strs) {
+        const count = new Array(26).fill(0);
+
+        for (let char of str) {
+            count[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+        }
+
+        const key = count.join('#');
+
+        if (!map.has(key)) {
+            map.set(key, []);
+        }
+
+        map.get(key).push(str);
+    }
+
+    return Array.from(map.values());
+
+    }
+}
