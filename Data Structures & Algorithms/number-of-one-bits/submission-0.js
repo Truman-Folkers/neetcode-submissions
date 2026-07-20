ea20f44cb0327c858b57999d72b3ad8ebe69ef00
Truman Folkers <37959465+Truman-Folkers@users.noreply.github.com>
2026-07-20 15:56:45 -0500

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.js
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {number} n - a positive integer
+     * @return {number}
+     */
+    hammingWeight(n) {
+        let count = 0;
+        for(let i = 0; i < 31; i++){
+            if(n & (1 << i)) count++;
+        }
+        return count;
+    }
+}
